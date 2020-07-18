5e5c39fe51d34b82465285f6a5eb80731d7a2374
Ritwik <ritwikkumar9857@gmail.com>
2020-07-18 13:06:22 +0530

    new update

---
+++ build/precache-manifest.8ca500605b54e7e22fe02f3b0762e84a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb6084361a41730c06bee9e0a1657a1",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "1222cbbf67b1c20cbc75",
+    "url": "/MyPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "dabbdf4df92ca72ea9b9",
+    "url": "/MyPortfolio/static/css/main.2abaaa77.chunk.css"
+  },
+  {
+    "revision": "1222cbbf67b1c20cbc75",
+    "url": "/MyPortfolio/static/js/2.bae83050.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolio/static/js/2.bae83050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabbdf4df92ca72ea9b9",
+    "url": "/MyPortfolio/static/js/main.1570fc91.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  },
+  {
+    "revision": "4ec7316053107e79c04a6edc62dd646e",
+    "url": "/MyPortfolio/static/media/img2.4ec73160.jpeg"
+  }
+]);
